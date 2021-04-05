b3e706630bb2ea1e44acef2d98d33ed820485af9
Johnny Wang <jywang@amazon.com>
2021-04-04 21:07:54 -0400

    adding starting and ending screens

---
+++ public/nickname.js
@@ -0,0 +1,17 @@
+let nickname = "";
+let nicknameInput = document.getElementById("nickname");
+
+function setNickname() {
+    localStorage.clear();
+    nickname = nicknameInput.value;
+    localStorage.setItem("nickname", nickname);
+}
+
+if(nicknameInput) {
+    nicknameInput.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("go-span").click();
+    }
+    });
+}
